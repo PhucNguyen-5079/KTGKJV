4b903b904837b6ed494adafd3543a560f5ebec78
phucz <pn70223@gmail.com>
2024-06-19 22:50:01 +0700

    Initial commit

---
+++ src/main/java/com/hutech/webbanhang/model/NhanVien.java
@@ -0,0 +1,37 @@
+package com.hutech.webbanhang.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "nhanVien")
+public class NhanVien {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String MaNv;
+    private String TenNv;
+    private String Phai;
+    private double Luong;
+    private String NoiSinh;
+
+
+    @ManyToOne
+    @JoinColumn(name = "Ma_Phong")
+    private PhongBan phongBan;;
+
+   /* public String getImgUrl() {
+        return ImgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        ImgUrl = imgUrl;
+    }
+
+    private String ImgUrl;*/
+
+}
